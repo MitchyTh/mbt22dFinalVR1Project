635a502fb46c8dbac9c2d1da7f93f40d2cc5dc21
Jake <user@FIH-COM23a111.fsu.edu>
2025-11-10 14:08:10 -0500

    Puts in enemy and zones

---
+++ Assets/enemyMoveScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class enemyMoveScript : MonoBehaviour
+{
+    public GameObject spawnZone;
+    public Transform endZone;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
